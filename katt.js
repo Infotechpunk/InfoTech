4426245acc81f758d3fdbcfe5ddd0144342d0700
Infotechpunk <93200532+Infotechpunk@users.noreply.github.com>
2023-01-31 10:26:28 +0100

    Add files via upload

---
+++ katt.js
@@ -0,0 +1,20 @@
+
+hei("Gjest");
+
+function hei(navn) {
+  document.writeln("< Mjau, " + navn + "! >                 ");
+  document.writeln("\\                           ");
+  document.writeln("  \\                           ");
+  document.writeln("    \\                           ");
+  document.writeln("      /\\___/\\                   ");
+  document.writeln("     ( o o  )                  ");
+  document.writeln("      \\ =0= /                   ");
+  console.log("< Mjau, " + navn + "! >                 ");
+  console.log("\\                           ");
+  console.log("  \\                           ");
+  console.log("    \\                           ");
+  console.log("      /\\___/\\                   ");
+  console.log("     (  o o  )                  ");
+  console.log("      \\ =0= /                   ");
+}
+document.body.style.whiteSpace = "pre";
---
+++ project.js
@@ -0,0 +1,40 @@
+let projects = [
+    {
+        name: "project one-HTMLTest",
+        tags: "#SimpleHTML-Test",
+        image: "img2.PNG",
+        url: "./projects/SimpleHTML/index.html"
+    },
+    {
+        name: "project two -test lenke til Google",
+        tags: "#Hyperlenke til Google",
+        image: "PeriodicT.png",
+        url: "https://www.google.no/"
+    },
+    {
+        name: "CurlingAnimation",
+        tags: "#HTML, #CSS",
+        image: "Curling.PNG",
+        url: "./projects/curling/index.html"
+
+    },
+
+    {
+        name: "Death Trip",
+        tags: "#fyllekjøring, #prosjekt",
+        image: "DeathTrip.png",
+        url: "./projects/vroom/index.html"
+
+    },
+
+    {
+        name: "Video på YouTube",
+        tags: "#Video, #Streaming",
+        image: "",
+        url:  "https://youtu.be/B_Hqiy4cqJA"
+
+    },
+
+
+    // +8 more
+]
